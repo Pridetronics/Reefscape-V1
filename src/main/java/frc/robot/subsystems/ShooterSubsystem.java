98d9d77304b5c90ea0eb899c9322f325814abe79
Jaxson-B <jaxson.barbian@pridetronics.com>
2025-02-15 13:22:22 -0700

    Set motors in ShooterSubsystem, still not Compileable.

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -6,10 +6,31 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkAbsoluteEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ShooterConstants;
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
+
+  //Setting new TalonFX motor
+  public final TalonFX shooterMotor = new TalonFX(ShooterConstants.kShooterMotorID);
+
+  //Set new SparkMax motor
+  public final SparkMax elevatorMotor = new SparkMax(ShooterConstants.kElevatorMotorID, MotorType.kBrushless);
+
+  //Encoder for our new motor
+  public final SparkAbsoluteEncoder elevatorAbsoluteEncoder = elevatorMotor.getAbsoluteEncoder();
+  
+  //Relative encoder for our motor
+  public final RelativeEncoder elevatorEncoder = elevatorMotor.getEncoder();
+
+
   public ShooterSubsystem() {}
 
   @Override
